86c282a8c6244c822fde3345b1b1295d4f5d31be
Florian Becker <becker_florian@online.de>
2021-07-31 12:41:44 +0200

    finished iteration 1

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,40 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+    title: {
+      type: String,
+      required: true, 
+      unique: true
+    },
+    level: {
+      type: String,
+      enum: "Easy Peasy"
+    }, 
+    ingredients: [{
+      type: String 
+    }], 
+    cuisine: {
+      type: String,
+      required: true
+    }, 
+    dishType: {
+      type: String,
+      enum: "main_course"
+    },
+    imnage: {
+      type: String,
+      enum: "https://images.media-allrecipes.com/images/75131.jpg"
+    }, 
+    duration: {
+      type: Number
+    }, 
+    creator: {
+      type: String
+    }, 
+    created: {
+      type: Date,
+      enum: 2021-07-31
+    }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
